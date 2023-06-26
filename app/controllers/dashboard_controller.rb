24a2684d1717dbbdb36d97dccaf7d921c0be7021
Rudzainy Rahman <rudzainy@gmail.com>
2023-06-26 20:55:44 +0800

    Add profile design page & setup user avatar.
    Also styled various pages.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,23 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_user, only: [:show]
 
   def index
     @should_render_navbar = true
+  end
+
+  def design
+    @should_render_navbar = true
+    
+  end
+
+  def show
+    redirect_to dashboard_path if @user.nil?
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'dashboard', to: "dashboard#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  get 'dashboard', to: 'dashboard#index'
+  get 'design', to: 'dashboard#design'
   root 'dashboard#index'
+
+  get ':id', to: 'dashboard#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
