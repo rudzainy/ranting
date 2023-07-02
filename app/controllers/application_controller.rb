4f0898d1b6fbd4228ae5c0030004d7e510a2a1a2
Rudzainy Rahman <rudzainy@gmail.com>
2023-07-02 21:43:37 +0800

    Remove theming feature.
    Also fixed public navbar settings.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :set_should_render_navbar
+  before_action :set_should_render_navbar_public
   before_action :set_hacker_variable
 
   def set_should_render_navbar
     @should_render_navbar = false
+  end
+
+  def set_should_render_navbar_public
+    if !user_signed_in? 
+      if params[:controller] == "links"
+        @should_render_navbar_public = false
+      else
+        @should_render_navbar_public = true
+      end
+    end
   end
 
   def set_hacker_variable
