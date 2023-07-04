a4f871f8d8c07958283f45bbb031101cee69346a
Rudzainy Rahman <rudzainy@gmail.com>
2023-07-04 09:22:36 +0800

    Misc UI updates.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,22 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user
 
   def index
     @should_render_navbar = true
-    @user = current_user
     @links = @user.links.order(position: :asc)
   end
 
   def design
     @should_render_navbar = true
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  
+  rescue StandardError
+    @user = nil
   end
 end
