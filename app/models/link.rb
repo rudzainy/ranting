59077dd3cc18d04db6387a0dd396095f443b42e2
Rudzainy Rahman <rudzainy@gmail.com>
2023-07-04 09:51:04 +0800

    Sanitize URL before save.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,11 @@
 class Link < ApplicationRecord
   belongs_to :user
   acts_as_list
+  before_save :sanitize_url
+
+  private
+
+  def sanitize_url
+    self.url = "https://#{self.url}" unless self.url=~/^https?:\/\//
+  end
 end
