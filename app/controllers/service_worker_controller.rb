a19cf512e9a5206c78b7b139b66465f7474ef597
Rudzainy Rahman <rudzainy@gmail.com>
2023-07-05 12:52:54 +0800

    [WIP] Error skipping user authentication.

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,6 +1,6 @@
 class ServiceWorkerController < ApplicationController
   protect_from_forgery except: :service_worker
-  skip_before_action :authenticate_user!  
+  # skip_before_action :authenticate_user!
   
   def service_worker
   end  
