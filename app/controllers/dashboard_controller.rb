fd3c45cd8e03120e4c4c71765fd57a5377d5c182
Faris <muhammadfaris@influx.com>
2023-09-24 17:49:09 +0800

    Convert dashboard#index to links#index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
 
-  def index
-    @should_render_navbar = true
-    @links = @user.links.order(position: :asc)
-  end
-
   def design
     @should_render_navbar = true
   end
