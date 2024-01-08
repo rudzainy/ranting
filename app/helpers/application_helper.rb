a3f82d3ee4e45d2841f0c2819099804043271909
Rudzainy Rahman <rudzainy@gmail.com>
2024-01-08 15:30:33 +0800

    Use card layout for links index on mobile browsers.
    - Table view is suitable for md screen sizes and above. This implements a solution to render different partilals for mobile device.
    - Currently the update link button is not clickable on touch.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_modal(title: "", body: "")
     render(partial: '/layouts/modal', locals: { title: title, body: body })
   end
+  
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
 end
