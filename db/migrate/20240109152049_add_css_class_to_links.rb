10f509e69e669c6b4dd7ea833987b801256f442e
Rudzainy Rahman <rudzainy@gmail.com>
2024-01-11 11:41:13 +0800

    Format 3x1 link card and other adventures!
    - Add placeholder image for link card.
    - Structure link card using BS card styles.
    - Set new links to have 2x1 style by default.

--- db/migrate/20240109152049_add_css_class_to_links.rb
+++ db/migrate/20240109152049_add_css_class_to_links.rb
@@ -1,5 +1,5 @@
 class AddCssClassToLinks < ActiveRecord::Migration[7.0]
   def change
-    add_column :links, :css_class, :integer, default: 0
+    add_column :links, :css_class, :integer, default: 3
   end
 end
