2dc5d423b905b330565a4f144d4afdd10d42c862
Rudzainy Rahman <rudzainy@gmail.com>
2024-01-19 02:04:48 +0800

    Fix for #37?
    https://stackoverflow.com/questions/64969386/undefined-method-cookie-value-for-impressionist-gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,19 @@ class UsersController < ApplicationController
     set_background_color
   end
 
+  def about
+    @user = User.find(1)
+
+    @should_render_navbar_public = true
+    impressionist(@user)
+    links = @user.links.where.not(url: 'https://').order(position: :asc)
+    @free_links = links.where(category: "free")
+    @social_links = links.where(category: "social")
+    @free_links.first.group.name
+    set_background_color
+    render template: "users/show"
+  end
+
   private
 
   def set_background_color
