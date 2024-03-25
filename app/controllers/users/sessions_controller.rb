e8d875562b02312f7e3b286997912d4d24fef136
Rudzainy Rahman <rudzainy@gmail.com>
2024-03-26 00:28:58 +0800

    Fallback redirect users to links#index after sign in.
    Instead of to root_path.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,7 +9,7 @@ class Users::SessionsController < Devise::SessionsController
   end
   
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
+    stored_location_for(resource_or_scope) || links_path
   end
 
   # GET /resource/sign_in
