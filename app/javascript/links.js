c3e2a79b9573b70efa2c672d934639cc65086672
Rudzainy Rahman <rudzainy@gmail.com>
2024-03-28 03:07:50 +0800

    Improve UI & UX for dragging cards.
    - Update dragdroptouch.
    - Other uiux improvements.

--- app/javascript/links.js
+++ app/javascript/links.js
@@ -1,13 +1,53 @@
-function handleDragStart(e) {
-  this.style.opacity = '0.4';
-}
+document.addEventListener('DOMContentLoaded', (event) => {
 
-function handleDragEnd(e) {
-  this.style.opacity = '1';
-}
+  function handleDragStart(e) {
+    this.style.opacity = '0.4';
+  
+    dragSrcEl = this;
+  
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
 
-let items = document.querySelectorAll('.container .link-card-draggable');
-items.forEach(function (item) {
-  item.addEventListener('dragstart', handleDragStart);
-  item.addEventListener('dragend', handleDragEnd);
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+
+    items.forEach(function (item) {
+      item.classList.remove('over');
+    });
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
+  
+  function handleDrop(e) {
+    e.stopPropagation(); // stops the browser from redirecting.
+  
+    if (dragSrcEl !== this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+  
+    return false;
+  }
+
+  let items = document.querySelectorAll('.link-card-draggable');
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart);
+    item.addEventListener('dragover', handleDragOver);
+    item.addEventListener('dragenter', handleDragEnter);
+    item.addEventListener('dragleave', handleDragLeave);
+    item.addEventListener('dragend', handleDragEnd);
+    item.addEventListener('drop', handleDrop);
+  });
 });
