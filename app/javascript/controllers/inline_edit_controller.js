f4d6e5aacae81d5c23ae9ea9cd3a778b53991db1
Rudzainy Rahman <rudzainy@gmail.com>
2024-04-30 11:41:04 +0800

    Bugfix.

--- app/javascript/controllers/inline_edit_controller.js
+++ app/javascript/controllers/inline_edit_controller.js
@@ -1,5 +1,5 @@
 // app/javascript/controllers/inline_edit_controller.js
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["groupName", "editForm"]
