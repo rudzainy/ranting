cd2be1472f9ba9c712d13591d9aff20e85b9abae
Rudzainy Rahman <rudzainy@gmail.com>
2024-05-07 18:11:41 +0800

    Add short_links#index for members.
    Free short links form is in short_links#free page.

--- app/controllers/short_links_controller.rb
+++ app/controllers/short_links_controller.rb
@@ -5,18 +5,30 @@ class ShortLinksController < ApplicationController
   def index
     @should_render_navbar = true
     # Find all current_user's short_links
-    @short_links = current_user.anonymous_short_links
+    @links = Link.all.where(category: 2, user_id: current_user.id)
+    @short_link = ShortLink.new
   end
 
   def new
     @short_link = ShortLink.new
   end
 
+  def free
+    @short_link = ShortLink.new
+  end
+
   def create
-    link = Link.create!(
-      url: url,
-      category: 2
-    )
+    if user_signed_in?
+      link = current_user.links.create!(
+        url: url,
+        category: 2,
+      )
+    else
+      link = Link.create!(
+        url: url,
+        category: 2
+      )
+    end
     
     @short_link = ShortLink.new
 
@@ -25,6 +37,10 @@ class ShortLinksController < ApplicationController
       statistics_token: SecureRandom.urlsafe_base64(4),
       url_token: SecureRandom.urlsafe_base64(4)
     )
+
+    if user_signed_in?
+      render turbo_stream: turbo_stream.prepend("short-links-container", partial: "short_links/table_row", locals: {link: link}, notice: "Link was successfully shorten.") 
+    end
     
   end
 
